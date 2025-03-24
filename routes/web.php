6064ab1713a3776cb68c0704e05f14c954eb624c
Matheus Nunes <contato.matthnuns@gmail.com>
2025-03-24 14:56:15 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function()
-{
+Route::get('/', function () {
     return view('index');
-});
 
+});
+ 
 Route::prefix('produtos')->group(function () {
     Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
 
